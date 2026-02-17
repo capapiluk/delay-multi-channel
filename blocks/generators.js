4c266310820dc869ad45528e37d6fc6144f7ab9f
CAP_DEV <127816113+capapiluk@users.noreply.github.com>
2026-02-17 20:11:11 +0700

    add

--- blocks/generators.js
+++ blocks/generators.js
@@ -8,7 +8,7 @@ Blockly.Python['relay_off'] = function (block) {
   const ch = block.getFieldValue('CH');
   Blockly.Python.definitions_['import_relay'] = 'import relay_4ch';
   return `relay_4ch.off(${ch})\n`;
-};
+};  
 
 Blockly.Python['relay_on_all'] = function () {
   Blockly.Python.definitions_['import_relay'] = 'import relay_4ch';
